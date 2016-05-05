eabe3a82db44170a043d9239c0ec5fa87770b612
kanocarra <emily.melhuish@gmail.com>
2016-05-05 12:29:00 +1200

    Added modules

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name findADealerApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the findADealerApp
+ */
+angular.module('findADealerApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
